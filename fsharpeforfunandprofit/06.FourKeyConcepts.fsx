c7251b0e7bdc4643af96e68be0bfbe968ea7d381
Mark Knight <dev@markfknight.com>
2017-06-12 23:14:57 +0100

    feat(06): algerbraic types

--- fsharpeforfunandprofit/06.FourKeyConcepts.fsx
+++ fsharpeforfunandprofit/06.FourKeyConcepts.fsx
@@ -7,3 +7,18 @@ let result = [1..10] |> List.map squareclone
 // functions taking other functions as parameters
 let execFunction aFunc aParam = aFunc aParam
 let result2 = execFunction square 12
+
+
+// Declare it
+type IntAndBool = {intPart: int; boolPart: bool}
+// use it
+let x = {intPart = 1; boolPart = false}
+
+// declare it
+type IntOrBool =
+    | IntChoice of int
+    | BoolChoice of bool
+
+// use it
+let y = IntChoice 42
+let z = BoolChoice true
