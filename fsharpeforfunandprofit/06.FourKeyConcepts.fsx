82dd9b96d81338b1702d328655efac0fb0ddf61f
Mark Knight <dev@markfknight.com>
2017-06-13 22:28:00 +0100

    feat(06): pattern matching

--- fsharpeforfunandprofit/06.FourKeyConcepts.fsx
+++ fsharpeforfunandprofit/06.FourKeyConcepts.fsx
@@ -22,3 +22,40 @@ type IntOrBool =
 // use it
 let y = IntChoice 42
 let z = BoolChoice true
+
+match x.boolPart with
+    | true -> "true"
+    | false -> "false"
+
+match x.intPart with
+    | 1 -> "1"
+    | 2 -> "2"
+    | _ -> "Out of range"
+
+match [1, 2, 3] with
+    | [] -> []
+    | first::rest ->
+        [first]
+
+type Shape =
+    | Circle of radius:int
+    | Rectangle of height:int * width:int
+    | Point of x:int * y:int
+    | Polygon of pointList:(int * int) list
+
+let draw shape =
+    match shape with
+    | Circle radius ->
+        printfn "The circle has radius of %d" radius
+    | Rectangle (height, width) ->
+        printfn "The rectangle is %d high and %d wide" height width
+    | Polygon points ->
+        printfn "The polygon is made of these points %A" points
+    | _ -> printfn "I don't recognise this shape"
+
+let circle = Circle(10)
+let rect = Rectangle(4, 5)
+let point = Point(2, 3)
+let polygon = Polygon([(1, 1); (2, 2); (3,3)])
+
+[circle; rect; polygon; point] |> List.iter draw
